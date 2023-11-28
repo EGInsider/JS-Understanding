02697de4b691574921613290f1416f2e5020e158
Divyansh Mishra <divyansh99mishra@gmail.com>
2023-11-28 02:16:41 +0000

    Operations in data conversions

---
+++ basics/dataConversion.js
@@ -0,0 +1,35 @@
+let first = Number("23")
+let second = Number("Divyansh")
+let third = Number(null)
+let fourth = Number(undefined)
+let fifth = Number(true)
+let sixth = Number(false)
+
+
+// console.log(typeof first, first)
+// console.log(typeof second, second)   // => NaN is also a number
+// console.log(typeof third, third)
+// console.log(typeof fourth, fourth)   // => NaN is also a number
+// console.log(typeof fifth, fifth)
+// console.log(typeof sixth, sixth)
+
+
+// ******************** Operations on Data *********************
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let counter = 1
+counter++;
+console.log(counter);
+++counter
+console.log(counter);
+counter = counter++     // => assignment has more precedence than postfix
+console.log(counter);
+counter = ++counter     // => assignment has less precedence than prefix
+console.log(counter);
---
+++ basics/falsyValues.js
@@ -0,0 +1,8 @@
+//There are 5 falsy values
+// 0, "", false, null, undefined
+
+console.log(Boolean(0)); 
+console.log(Boolean(false));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
