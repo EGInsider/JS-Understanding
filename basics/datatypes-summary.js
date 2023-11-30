85601ac0876e8bd2f600e32e4f239854e0de73e8
Divyansh Mishra <divyansh99mishra@gmail.com>
2023-11-30 02:37:33 +0000

    Memory Allocation

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -20,8 +20,29 @@ let myFunction = function(){
    console.log(myInfo.name)
 }
 
-console.log(typeof bigNumber, bigNumber);
-console.log(typeof id, id);
-console.log(typeof bikes, bikes);
-console.log(typeof myInfo, myInfo);
-console.log(typeof myFunction, myFunction);
+// console.log(typeof bigNumber, bigNumber);
+// console.log(typeof id, id);
+// console.log(typeof bikes, bikes);
+// console.log(typeof myInfo, myInfo);
+// console.log(typeof myFunction, myFunction);
+
+
+//  ********************************* Memory Allocation in JS **************************************
+
+// Stack (Primitive types)
+// In stack memory we always get a copy of the original value and a change in copy will not affect the original value
+let name1 = "Divyansh"
+let name2 = name1
+name2 = "Mishra Ji"
+
+console.log(name1);  // original value remains intact
+console.log(name2);
+
+// Heap (Non Primitive Types/ Reference types)
+// In heap memory we always get a reference of the original value and any change will result in changes in the original value
+let newInfo = myInfo
+newInfo.age = 27
+
+console.log(myInfo); // original value changed
+console.log(newInfo);
+
