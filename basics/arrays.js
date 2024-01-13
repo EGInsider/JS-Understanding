c6a6409c9574391b557e9ce101dae760c364687d
Divyansh Mishra <133009617+EGInsider@users.noreply.github.com>
2024-01-13 15:13:22 +0000

    array basics

---
+++ basics/arrays.js
@@ -0,0 +1,54 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const myArr = [1,2,3,4,5];
+
+// an array can store elements of different datatypes as well
+// const myArr = [1,'Divyansh', true, null, undefined, 23, 97]
+
+// declaring array using new keyword
+// const myArr = new Array(1,2,3,4,5);
+
+// ----------------------Array Methods-------------------------
+
+// Addition and Deletion
+// myArr.push(6)
+// myArr.unshift()
+// myArr.shift()
+// myArr.pop()
+// console.log(myArr);
+
+// splice(index, number, items[])
+// splice(index elements need to be deleted from, number of elements to be deleted, elements need to be added(if not deleting))
+// also returns a new array ontaining the elements that were deleted
+// myArr.splice(myArr.length,0,7,8)
+// console.log(myArr.splice(myArr.length,0,7,8));
+
+// Searching
+// console.log(myArr.indexOf(5)); 
+// console.log(myArr.indexOf(5,5));
+// const myArr2 = [1,5,23,6,97,5]
+// console.log(myArr2.lastIndexOf(5));
+// console.log(myArr2.includes(23));
+// console.log(myArr2.includes(23, 3));
+
+// joining array elements as string
+// const arr = ['Hi',"I'm",'Divyansh','and',"I'm", 23]
+// console.log(arr.join(" "));
+
+// Merging two(or more) arrays
+
+// concat returns a new array
+// const mergedArr = myArr.concat([7,8])
+// console.log(mergedArr); 
+
+// merging using spread operator
+// const veg = ['paneer butter masala', 'paneer tikka', 'veg-roll']
+// const non_veg = ['butter chicken', 'chicken tikka', 'chicken-roll']
+// const cuisine = [...veg, ...non_veg]
+// console.log(cuisine);
+
+// converting multidimensional arrays into one-dimensional array
+const multiArr = [1,2,3,[4,5],6,[7,8,[9,10,[11,12]]]]
+console.log(multiArr.flat(Infinity));
+
+
