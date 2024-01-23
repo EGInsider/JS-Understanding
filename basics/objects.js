32855764e7af83179fc66752a0403da1ec9dd1b8
Divyansh Mishra <133009617+EGInsider@users.noreply.github.com>
2024-01-23 13:44:30 +0000

    objects part 2

--- basics/objects.js
+++ basics/objects.js
@@ -23,3 +23,28 @@ const myself = {
 // console.log(myself);
 // console.log(myself.greet());
 // console.log(myself.greet);
+
+// singleton object
+const singleObject = new Object()
+singleObject.id = 23
+
+// nesting in objects
+singleObject.creds = {
+    callname : "Mr_Unknownymous",
+    callsign : "Phoenix",
+    classifiedData : {
+        fullname : "Divyansh Mishra",
+        age : 26,
+    }
+}
+
+// console.log(`hello ${singleObject.creds.callname} callsign: ${singleObject.creds.callsign} aka ${singleObject.creds.classifiedData.fullname}`);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+// console.log({obj1, obj2}); // wrong
+// console.log({...obj1, ...obj2}); // right
+
+// object.assign(target, source1, source2,.....sourceN)
+// console.log(Object.assign({}, obj1, obj2)); // right
